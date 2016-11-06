68c0ff76b93feebb6c5a8ee4bd42a2e1cfd1e57a
Peyton Casper <peyton@Peytons-MBP.attlocal.net>
2016-11-06 10:00:05 -0600

    Removed all the search tree code and added in BFS + Supporting methods

--- Assignment 1/main.c
+++ Assignment 1/main.c
@@ -4,26 +4,6 @@
 #include <stdbool.h>
 #include <math.h>
 
-struct node {
-    struct node* leafs;
-    struct node* parent;
-
-    int* coords;
-
-    int distance;
-    int depth;
-
-    bool val;
-    bool valid;
-};
-struct node generate_search_tree(int start_node[2], int current_node[2], int goal[2], int depth, int max_depth, bool map[32][20], int** coordinates_visted, int* len);bool coordinates_check(int** coordinate_list, int* len, int coordinate[2]);
-int count_tree(struct node root, int count);
-void print_memory_usage(struct node root);
-void print_searched_map(int** coordinates_visted, int len);
-struct node* UCS(struct node root);
-void enqueue(struct node** queue, struct node* root, int* len);
-struct node dequeue(struct node** queue, int* len);
-void print_tree(struct node root, int starting_point[2]);
 /* ------------------------ BEGINNING OF PROVIDED CODE  -------------------------- */
 
 #define MAX_OBSTACLES 25 /* maximum number of obstacles */
@@ -64,6 +44,12 @@ double goal[2] = {3.658, 1.829}; /* goal location */
 void print_map();
 int float_point_to_grid_coords(float point);
 
+int** enqueue(int** queue, int element[2], int* length);
+int** dequeue(int** queue, int* length);
+bool BFS(int start_pos[2], int goal_pos[2], int** coordinates_visited);
+bool coordinates_check(int** coordinate_list, int* len, int coordinate[2]);
+bool is_goal(int c_x, int c_y, int g_x, int g_y);
+int** get_moves(int current_pos[2], int* num_moves);
 /* ------------------------ END OF PROVIDED CODE  -------------------------- */
 
 #define GRID_WIDTH  16
@@ -81,10 +67,10 @@ int iterationDepth = 5;
 double robotDiameter = 0.305;
 
 int main(void){
-    
+    int i, j = 0;
     // Mark map edges as not safe to navigate.
-    for(int i = 0; i < ( GRID_WIDTH * GRID_SEG_PER_TILE ); i++) {
-        for(int j = 0; j < ( GRID_HEIGHT * GRID_SEG_PER_TILE ); j++) {
+    for(i = 0; i < ( GRID_WIDTH * GRID_SEG_PER_TILE ); i++) {
+        for(j = 0; j < ( GRID_HEIGHT * GRID_SEG_PER_TILE ); j++) {
 
             if (
                     ( j == 0 ) ||
@@ -102,15 +88,16 @@ int main(void){
     int tiles_to_cover = (int)roundf((TILE_SIZE/2 + robotDiameter/2)/TILE_SIZE) * 2;
     //printf("Tiles to cover: %d \n", tiles_to_cover);
     // Mark Obsticles
-    for(int i = 0; i < num_obstacles; i++)
+    for(i = 0; i < num_obstacles; i++)
     {
+        int k,l = 0;
         int aprox_x = float_point_to_grid_coords(obstacle[i][0]);
         int aprox_y = float_point_to_grid_coords(obstacle[i][1]);
 
         obstacleArray[aprox_x][aprox_y] = true;
 
-        for(int k = aprox_x - tiles_to_cover; k <= aprox_x + tiles_to_cover; k++){
-            for(int l = aprox_y - tiles_to_cover; l <= aprox_y + tiles_to_cover; l++){
+        for(k = aprox_x - tiles_to_cover; k <= aprox_x + tiles_to_cover; k++){
+            for(l = aprox_y - tiles_to_cover; l <= aprox_y + tiles_to_cover; l++){
                 if ((k >= 0) && (k < ( GRID_WIDTH * GRID_SEG_PER_TILE)) && (l >= 0) && (l < ( GRID_HEIGHT * GRID_SEG_PER_TILE)) )
                     obstacleArray[k][l] = true;
             }
@@ -123,58 +110,15 @@ int main(void){
 
     int starting_cords[2] = {float_point_to_grid_coords(start[0]), float_point_to_grid_coords(start[1])};
     int goal_cords[2] = {float_point_to_grid_coords(goal[0]), float_point_to_grid_coords(goal[1])};
-    int** coordinates_visited = malloc(sizeof(double*) * 760);
+    int coordinates_visited[GRID_WIDTH * GRID_SEG_PER_TILE * GRID_HEIGHT][2];
     int length = 0;
-    //142
-    struct node root = generate_search_tree(starting_cords, starting_cords, goal_cords, 0, 5001, obstacleArray, coordinates_visited, &length);
-//    print_searched_map(coordinates_visited, length);
-//    print_tree(root, goal_cords);
-    UCS(root);
-    print_searched_map(coordinates_visited, length);
+
+    if(BFS(starting_cords, goal_cords, coordinates_visited))
+        printf("Solution found");
+    else
+        printf("No Solution");
+
     return 0;
-}
-void print_tree(struct node root, int goal[2]){
-    root.distance = abs(root.coords[0] - goal[0]) + abs(root.coords[1] - goal[1]);
-    printf("Distance: %d\n", root.distance);
-
-    for(int i = 0; i < 4; i++){
-        if(root.leafs[i].valid)
-        {
-            print_tree(root.leafs[i], goal);
-        }
-    }
-}
-
-void enqueue(struct node** queue, struct node* root, int* len){
-    (*len)++;
-    realloc(queue, sizeof(struct node) * (*len));
-    queue[(*len) - 1] = root;
-}
-struct node dequeue(struct node** queue, int* len){
-    (*len)--;
-    struct node val = *queue[*len];
-    realloc(queue, sizeof(struct node*) * (*len));
-
-    return val;
-}
-
-void print_memory_usage(struct node root)
-{
-    int struct_size = sizeof(root.valid) + sizeof(root.val) + (sizeof(root.coords) * 2) + sizeof(root.depth) + (sizeof(root.leafs) * 4);
-    printf("Search Tree Memory Usage: %d", struct_size * count_tree(root, 0));
-}
-
-int count_tree(struct node root, int count){
-    count++;
-    for(int i = 0; i < 4; i++)
-    {
-        if(root.leafs[i].valid)
-        {
-            count = count_tree(root.leafs[i], count);
-        }
-    }
-    return count;
-
 }
 
 void print_map(){
@@ -196,142 +140,120 @@ void print_map(){
         printf("\n");
     } 
 }
-void print_searched_map(int** coordinates_visted, int len){
-    printf("\n X ->\tStart Coords: (%d, %d)", float_point_to_grid_coords(start[0]), float_point_to_grid_coords(start[1]) );
-    printf("\n O ->\tGoal Coords: (%d, %d)", float_point_to_grid_coords(goal[0]), float_point_to_grid_coords(goal[1]) );
-    printf("\n + ->\tOccupied Cell\n\n");
 
-    for(int j = (GRID_HEIGHT * GRID_SEG_PER_TILE) - 1; j >= 0 ; j--) {
-        for(int i = 0; i < (GRID_WIDTH * GRID_SEG_PER_TILE); i++) {
-            int node[2] = {i, j};
-            if ( i == float_point_to_grid_coords(start[0]) && j == float_point_to_grid_coords(start[1])){
-                printf("X ");
-            }
-            else if ( i == float_point_to_grid_coords(goal[0]) && j == float_point_to_grid_coords(goal[1])) {
-                printf("O ");
-            }
-            else if (coordinates_check(coordinates_visted, &len, node)){
-                printf("- ");
-            }
-            else
-                printf("%c ", obstacleArray[i][j] ? '+' : ' ');
-        }
-        printf("\n");
-    }
-}
 int float_point_to_grid_coords(float point){
         return (int)roundf(point/TILE_SIZE) * GRID_SEG_PER_TILE;
 }
 
-struct node** insert_priority_queue(struct node** queue, struct node* element, int length){
-    realloc(queue, sizeof(struct node*) * length + 1);
-
-    for(int i = 0; i < length; i++){
-        if(queue[i]->distance >= element->distance){
-            for(int j = i; j < length; j++)
-            {
-                queue[j+1] = queue[j];
-            }
-            queue[i] = element;
-            return queue;
-        }
+int** enqueue(int** queue, int element[2], int* length){
+    (*length)++;
+    int** temp = realloc(queue, sizeof(int*) * (*length));
+    if (temp==NULL) {
+        printf("Error allocating memory");
     }
-    queue[length] = element;
+    else
+    {
+        queue = temp;
+    }
+
+    queue[(*length) - 1] = element;
     return queue;
 }
-struct node* UCS(struct node root){
-    int length = 1;
-    struct node** queue = malloc(sizeof(struct node*) * length);
+int** dequeue(int** queue, int* length){
+    int i = 0;
+    for(i = 1; i < (*length); i++)
+    {
+        queue[i - 1] = queue[i];
+    }
 
-    queue[0] = &root;
-    queue = insert_priority_queue(queue, &root.leafs[0], length);
+    (*length)--;
 
-    printf("%d\n", queue[0]->distance);
-    printf("%d\n", queue[1]->distance);
+    queue = realloc(queue, sizeof(int*) * (*length));
+    return queue;
 }
+bool BFS(int start_pos[2], int goal_pos[2], int** coordinates_visited){
+    // If starting position is the goal, return true;
+    if(is_goal(start_pos[0], start_pos[1], goal_pos[0], goal_pos[1]))
+        return true;
+    // Initialize queue, queue_length, and visted_length
+    int q_length = 1, v_length = 0;
+    int** queue = malloc(sizeof(int*) * q_length);
 
-struct node generate_search_tree(int start_node[2], int current_node[2], int goal[2], int depth, int max_depth, bool map[32][20], int** coordinates_visted, int* len){
-
-
-    struct node root;
-    root.val = map[current_node[0]][current_node[1]];
-    root.leafs = malloc(sizeof(struct node) * 4);
-    root.valid = true;
-
-    root.coords = malloc(sizeof(int) * 2);
-    root.coords[0] = current_node[0];
-    root.coords[1] = current_node[1];
-
+    // Set the initial queue element equal to the start nodes
+    queue[q_length - 1] = start_pos;
+    int num_moves, i = 0;
 
 
+    // TODO
+    // If this runs/works on the NXT then we have to keep track of the paths
+    // Essentially this means enqueuing int* arrays that contain the next possible move
+    // Currently this will simply enqueue the next node, but you will need to malloc a suitable array and load the path
+    // Reference: http://stackoverflow.com/questions/8922060/how-to-trace-the-path-in-a-breadth-first-search
 
-    root.distance = 99999;
-    coordinates_visted[*len] = malloc(sizeof(int) * 2);
-    coordinates_visted[*len][0] = current_node[0];
-    coordinates_visted[*len][1] = current_node[1];
-    (*len)++;
 
+    // Loop till the queue is empty
+    while(q_length != 0){
+        // Dequeue the first element
+        int* head = queue[0];
+        queue = dequeue(queue, &q_length);
+        // Add the element to the visited list
+        coordinates_visited[v_length] = head;
 
-        if(current_node[0] - 1 >= 0 && map[current_node[0] - 1][current_node[1]] == 0)
+        // Get all the possible moves from the tile currently being looked at
+        int** moves = get_moves(head, &num_moves);
+        for(i = 0; i < num_moves; i++)
         {
-            int new_node[2] = {current_node[0] - 1, current_node[1]};
-
-            if(!coordinates_check(coordinates_visted, len, new_node)) {
-                struct node top = generate_search_tree(start_node, new_node, goal, depth + 1, max_depth, map, coordinates_visted, len);
-                root.leafs[0] = top;
-                top.parent = &root;
+            // If the tile is free
+            if(!coordinates_check(coordinates_visited,&v_length, moves[i]))
+            {
+                // If the tile is the goal node then return true
+                if(is_goal(moves[i][0], moves[i][1], goal[0], goal[1]))
+                {
+                    return true;
+                }
+                // If the tile is free then enqueue that move
+                queue = enqueue(queue, moves[i], &q_length);
             }
-            else
-                root.leafs[0].valid = false;
+
         }
-        else
-            root.leafs[0].valid = false;
-        if(current_node[0] + 1 <= 37 && map[current_node[0] + 1][current_node[1]] == 0)
-        {
-            int new_node[2] = {current_node[0] + 1, current_node[1]};
-            if(!coordinates_check(coordinates_visted, len, new_node)) {
+        num_moves = 0;
+    }
+    // All tiles traversed return false, no path
+    return false;
+}
+int** get_moves(int current_pos[2], int* num_moves)
+{
+    int** moves = malloc(sizeof(int*) * 4);
 
-                struct node right = generate_search_tree(start_node, new_node, goal, depth + 1, max_depth, map, coordinates_visted, len);
-                root.leafs[1] = right;
-                right.parent = &root;
+    if(obstacleArray[current_pos[0]-1][current_pos[1]] == false) {
+        moves[(*num_moves)] = malloc(sizeof(int) * 2);
+        moves[(*num_moves)][0] = current_pos[0] - 1;
+        moves[(*num_moves)][1] = current_pos[1];
+        (*num_moves)++;
+    }
+    if(obstacleArray[current_pos[0]][current_pos[1] - 1] == false) {
+        moves[(*num_moves)] = malloc(sizeof(int) * 2);
+        moves[(*num_moves)][0] = current_pos[0];
+        moves[(*num_moves)][1] = current_pos[1] - 1;
+        (*num_moves)++;
+    }
+    if(obstacleArray[current_pos[0] + 1][current_pos[1]] == false) {
+        moves[(*num_moves)] = malloc(sizeof(int) * 2);
+        moves[(*num_moves)][0] = current_pos[0] + 1;
+        moves[(*num_moves)][1] = current_pos[1];
+        (*num_moves)++;
+    }
+    if(obstacleArray[current_pos[0]][current_pos[1] + 1] == false) {
+        moves[(*num_moves)] = malloc(sizeof(int) * 2);
+        moves[(*num_moves)][0] = current_pos[0];
+        moves[(*num_moves)][1] = current_pos[1] + 1;
+        (*num_moves)++;
+    }
 
-            }
-            else
-                root.leafs[1].valid = false;
-        }
-        else
-            root.leafs[1].valid = false;
-        if(current_node[1] - 1 >= 0 && map[current_node[0]][current_node[1] - 1] == 0)
-        {
-            int new_node[2] = {current_node[0], current_node[1] - 1};
-            if(!coordinates_check(coordinates_visted, len, new_node)) {
-
-                struct node left = generate_search_tree(start_node, new_node, goal, depth + 1, max_depth, map, coordinates_visted, len);
-                root.leafs[2] = left;
-                left.parent = &root;
-            }
-            else
-                root.leafs[2].valid = false;
-        }
-        else
-            root.leafs[2].valid = false;
-        if(current_node[1] + 1 <= 37 && map[current_node[0]][current_node[1] + 1] == 0)
-        {
-            int new_node[2] = {current_node[0], current_node[1] + 1};
-            if(!coordinates_check(coordinates_visted, len, new_node)) {
-
-                struct node bottom = generate_search_tree(start_node, new_node, goal, depth + 1, max_depth, map, coordinates_visted, len);
-                root.leafs[3] = bottom;
-                bottom.parent = &root;
-
-            }
-            else
-                root.leafs[3].valid = false;
-        }
-        else
-            root.leafs[3].valid = false;
-
-    return root;
+    return moves;
+}
+bool is_goal(int c_x, int c_y, int g_x, int g_y){
+    return c_x == g_x && c_y == g_y;
 }
 bool coordinates_check(int** coordinate_list, int* len, int coordinate[2]){
     for(int i = 0; i < *len; i++)
